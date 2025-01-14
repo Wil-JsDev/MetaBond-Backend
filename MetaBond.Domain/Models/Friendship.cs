db9bf671f2d72229b9202325c2d7d526b77f24f7
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-13 21:44:38 -0400

    chore: change name project

---
+++ MetaBond.Domain/Models/Friendship.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    namespace MetaBond.Domain.Models;
+
+    public sealed class Friendship
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+           
+        public enum Status { Pendiete, Aceptada, Bloqueada }    
+
+        public DateTime? CreateAt { get; set; }
+    }
