a370964e57796e21af1a397b6276fbd0e2940f60
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-17 22:31:36 -0400

    feat: add repository implementations for data access
    implemented repository interfaces for managing entities

---
+++ MetaBond.Infrastructure.Persistence/Repository/FriendshipRepository.cs
@@ -0,0 +1,34 @@
+﻿using MetaBond.Application.Interfaces.Repository;
+using MetaBond.Application.Pagination;
+using MetaBond.Domain.Models;
+using MetaBond.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetaBond.Infrastructure.Persistence.Repository
+{
+    public class FriendshipRepository : GenericRepository<Friendship>, IFriendshipRepository
+    {
+        public FriendshipRepository(MetaBondContext metaBondContext) : base(metaBondContext)
+        {
+        }
+
+        public async Task<PagedResult<Friendship>> GetPagedFriendshipAsync(int pageNumber, int pageZize, CancellationToken cancellationToken)
+        {
+            var totalRecord = await _metaBondContext.Set<Friendship>().AsNoTracking().CountAsync(cancellationToken);
+
+            var pagedFriendship = await _metaBondContext.Set<Friendship>().AsNoTracking()
+                                  .OrderBy(f => f.Id)
+                                  .Skip((pageNumber - 1) * pageZize)
+                                  .Take(pageZize)
+                                  .ToListAsync(cancellationToken);
+
+            var pagedResponse = new PagedResult<Friendship>(pagedFriendship,pageNumber,pageZize, totalRecord);
+            return pagedResponse;
+        }
+    }
+}
---
+++ MetaBond.Infrastructure.Persistence/Repository/ParticipationInEventRepository.cs
@@ -0,0 +1,18 @@
+﻿using MetaBond.Application.Interfaces.Repository;
+using MetaBond.Domain.Models;
+using MetaBond.Infrastructure.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetaBond.Infrastructure.Persistence.Repository
+{
+    public class ParticipationInEventRepository : GenericRepository<ParticipationInEvent>, IParticipationInEventRepository
+    {
+        public ParticipationInEventRepository(MetaBondContext metaBondContext) : base(metaBondContext)
+        {
+        }
+    }
+}
