69929c7b220a10f333559ca5f0cbc6edc1d8961e
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-17 22:26:49 -0400

    feat: add pagination data

---
+++ MetaBond.Application/Pagination/PagedResult.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetaBond.Application.Pagination
+{
+    public class PagedResult<T>
+    {
+        public PagedResult()
+        {
+
+        }
+
+        public PagedResult(IEnumerable<T>? items, int totalItems, int currentPage, int pageSize)
+        {
+            Items = items;
+            TotalItems = totalItems;
+            CurrentPage = currentPage;
+            TotalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
+        }
+
+        public IEnumerable<T>? Items { get; set; }
+
+        public int TotalItems { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public int CurrentPage { get; set; }
+
+    }
+}
