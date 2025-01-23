470f5e554f8998418c0ef3a309083447c1e4100a
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-23 16:31:18 -0400

    feat(CQRS): implement commands and queries structure
    added new commands and queries following the CQRS pattern
    
    structured the application to separate write (commands) and read (queries) operations

---
+++ MetaBond.Application/Abstractions/Messaging/IQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using MetaBond.Application.Utils;
+
+namespace MetaBond.Application.Abstractions.Messaging
+{
+    public interface IQuery<TResponse> : IRequest<ResultT<TResponse>>;
+}
---
+++ MetaBond.Application/Abstractions/Messaging/IQueryHandler.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using MetaBond.Application.Utils;
+
+namespace MetaBond.Application.Abstractions.Messaging
+{
+    public interface IQueryHandler<in TQuery, TResponse> :
+        IRequestHandler<TQuery, ResultT<TResponse>>
+        where TQuery : IQuery<TResponse>;
+}
