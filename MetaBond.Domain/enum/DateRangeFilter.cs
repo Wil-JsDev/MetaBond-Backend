114f4fd7691b9b3634ec3b9c311fcaf02c6b98eb
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-27 10:35:31 -0400

    feat: add enum date range filter

---
+++ MetaBond.Domain/enum/DateRangeFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetaBond.Domain
+{
+    public enum DateRangeFilter
+    {
+        LastDay = 0,
+        ThreeDays = 1,
+        LastWeek = 2
+    }
+}
