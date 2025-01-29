5c856f24efe4051b9b4821c83974f06e0b1c2e16
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-29 19:40:28 -0400

    feat: add friendship command

---
+++ MetaBond.Application/Feature/Friendship/Command/Create/CreateFriendshipCommand.cs
@@ -0,0 +1,11 @@
+﻿using MetaBond.Application.Abstractions.Messaging;
+using MetaBond.Application.DTOs.Friendship;
+using MetaBond.Domain;
+
+namespace MetaBond.Application.Feature.Friendship.Command.Create
+{
+    public sealed class CreateFriendshipCommand : ICommand<FriendshipDTos>
+    {
+        public Status Status { get; set; }
+    }
+}
---
+++ MetaBond.Application/Feature/Friendship/Command/Update/UpdateFrienshipCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using MetaBond.Application.Abstractions.Messaging;
+using MetaBond.Application.DTOs.Friendship;
+using MetaBond.Application.Interfaces.Repository;
+using MetaBond.Application.Utils;
+
+namespace MetaBond.Application.Feature.Friendship.Command.Update
+{
+    internal sealed class UpdateFrienshipCommandHandler : ICommandHandler<UpdateFriendshipCommand, FriendshipDTos>
+    {
+        private readonly IFriendshipRepository _friendshipRepository;
+
+        public UpdateFrienshipCommandHandler(IFriendshipRepository friendshipRepository)
+        {
+            _friendshipRepository = friendshipRepository;
+        }
+
+        public async Task<ResultT<FriendshipDTos>> Handle(UpdateFriendshipCommand request, CancellationToken cancellationToken)
+        {
+            var friendship = await _friendshipRepository.GetByIdAsync(request.Id);
+            if (friendship != null)
+            { 
+                friendship.Status = request.Status;
+                Domain.Models.Friendship friendshipModel = new()
+                {
+                    Id = friendship.Id,
+                    Status = friendship.Status,
+                    CreateAt = friendship.CreateAt
+                };
+                await _friendshipRepository.UpdateAsync(friendshipModel,cancellationToken);
+
+                FriendshipDTos friendshipDTos = new
+                (
+                    FriendshipId: friendshipModel.Id,
+                    Status: friendshipModel.Status,
+                    CreatedAt: friendshipModel.CreateAt
+                );
+
+                return ResultT<FriendshipDTos>.Success(friendshipDTos);
+
+            }
+
+            return ResultT<FriendshipDTos>.Failure(Error.NotFound("404",$"{request.Id} not found")); 
+            
+        }
+    }
+}
