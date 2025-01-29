0c252db402c7b6411e4e6ad9440074a96f1da5c6
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-29 19:38:11 -0400

    feat: add past date range type enum

---
+++ MetaBond.Domain/enum/PastDateRangeType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetaBond.Domain
+    {
+    public enum PastDateRangeType
+    {
+        BeforeToday = 0,
+        BeforeWeek = 1,
+        BeforeMonth = 2,
+        BeforeYear = 3
+    }
+}
