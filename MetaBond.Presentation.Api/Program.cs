4705e6bb46ec9899976259b31a2f8914bdfbb674
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-02-01 18:27:52 -0400

    feat: configure Serilog logging in Program.cs for enhanced error tracking and monitoring

--- MetaBond.Presentation.Api/Program.cs
+++ MetaBond.Presentation.Api/Program.cs
@@ -1,32 +1,57 @@
 using MetaBond.Infrastructure.Persistence;
 using MetaBond.Infrastructure.Shared;
 using MetaBond.Application;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+             .WriteTo.Console()
+             .CreateLogger();
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddPersistence(builder.Configuration);
-builder.Services.AddShared(builder.Configuration);
-builder.Services.AddApplicationLayer();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("starting server");
+     
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((context, loggerConfiguration) =>
+    {
+        loggerConfiguration.WriteTo.Console();
+        loggerConfiguration.ReadFrom.Configuration(context.Configuration);
+    });
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddPersistence(builder.Configuration);
+    builder.Services.AddShared(builder.Configuration);
+    builder.Services.AddApplicationLayer();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex,"server terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
