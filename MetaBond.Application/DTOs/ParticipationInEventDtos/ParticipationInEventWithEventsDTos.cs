9cc7ad7cde6bf49280368142091a1d35f89707f5
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-02-02 10:09:37 -0400

    feat: add participation in event dtos

---
+++ MetaBond.Application/DTOs/ParticipationInEventDtos/ParticipationInEventWithEventsDTos.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MetaBond.Application.DTOs.ParticipationInEventDtos
+{
+    public sealed record ParticipationInEventWithEventsDTos
+    (
+        Guid? ParticipationInEventId,
+        Guid? EventId,
+        ICollection<Domain.Models.Events> Events
+    );
+}
