a5d4f53b04444546e80f34b5104752d59261f8a1
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-02-03 19:42:30 -0400

    feat: add Posts commands and handlers for improved functionality

---
+++ MetaBond.Application/Feature/Posts/Commands/Create/CreatePostsCommand.cs
@@ -0,0 +1,19 @@
+﻿using MetaBond.Application.Abstractions.Messaging;
+using MetaBond.Application.DTOs.Posts;
+using Microsoft.AspNetCore.Http;
+
+namespace MetaBond.Application.Feature.Posts.Commands.Create
+{
+    public sealed class CreatePostsCommand : ICommand<PostsDTos>
+    {
+        public string? Title { get; set; }
+
+        public string? Content { get; set; }
+        
+        public string? ImageUrl { get; set; }
+
+        public Guid? CommunitiesId { get; set; }
+
+        public IFormFile? ImageFile { get; init; }
+    }
+}
---
+++ MetaBond.Application/Feature/Posts/Commands/Delete/DeletePostsCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MetaBond.Application.Abstractions.Messaging;
+using MetaBond.Application.Interfaces.Repository;
+using MetaBond.Application.Utils;
+using Microsoft.Extensions.Logging;
+
+namespace MetaBond.Application.Feature.Posts.Commands.Delete
+{
+    internal sealed class DeletePostsCommandHandler : ICommandHandler<DeletePostsCommand, Guid>
+    {
+        private readonly IPostsRepository _postsRepository;
+        private readonly ILogger<DeletePostsCommandHandler> _logger;
+
+        public DeletePostsCommandHandler(IPostsRepository postsRepository, ILogger<DeletePostsCommandHandler> logger)
+        {
+            _postsRepository = postsRepository;
+            _logger = logger;
+        }
+
+        public async Task<ResultT<Guid>> Handle(DeletePostsCommand request, CancellationToken cancellationToken)
+        {
+            var posts = await _postsRepository.GetByIdAsync(request.PostsId);
+            if (posts == null)
+            {
+                _logger.LogError("");
+
+                return ResultT<Guid>.Failure(Error.NotFound("404", $"{request.PostsId} not found"));
+            }
+
+            await _postsRepository.DeleteAsync(posts, cancellationToken);
+
+            _logger.LogInformation("");
+
+            return ResultT<Guid>.Success(posts.Id);
+        }
+    }
+}
