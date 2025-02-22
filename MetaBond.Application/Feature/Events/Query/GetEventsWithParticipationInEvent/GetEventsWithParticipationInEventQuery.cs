5433fe79cf0c140aed82ee1e5f0361a2ea1ea96e
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-02-22 18:47:15 -0400

    feat: add events with participation in event handler

---
+++ MetaBond.Application/Feature/Events/Query/GetEventsWithParticipationInEvent/GetEventsWithParticipationInEventQuery.cs
@@ -0,0 +1,11 @@
+﻿
+using MetaBond.Application.Abstractions.Messaging;
+using MetaBond.Application.DTOs.Events;
+
+namespace MetaBond.Application.Feature.Events.Query.GetEventsWithParticipationInEvent
+{
+    public sealed class GetEventsWithParticipationInEventQuery : IQuery<EventsWithParticipationInEventsDTos>
+    {
+        public Guid? EventsId { get; set; }
+    }
+}
