ebee4a940ec73f5b19ed1b75157a5a7965f1bf86
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-02-22 21:37:50 -0400

    feat: add join method

--- MetaBond.Application/Interfaces/Repository/IParticipationInEventRepository.cs
+++ MetaBond.Application/Interfaces/Repository/IParticipationInEventRepository.cs
@@ -1,15 +1,12 @@
 ﻿using MetaBond.Application.Pagination;
 using MetaBond.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MetaBond.Application.Interfaces.Repository
 {
     public interface IParticipationInEventRepository : IGenericRepository<ParticipationInEvent>
     {
         Task<PagedResult<ParticipationInEvent>> GetPagedParticipationInEventAsync(int pageNumber, int pageSize, CancellationToken cancellationToken);
+
+        Task<IEnumerable<ParticipationInEvent>> GetEventsAsync(Guid participationInEventId, CancellationToken cancellationToken);
     }
 }
