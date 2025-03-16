8e4786cb03e38bbf4efadc9eb19cb09e326be913
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-03-16 15:24:48 -0400

    refactor: implement date range filter validation logic
    - Added validation for the DateRangeFilter enum to handle specific cases like 'LastDay', 'ThreeDays', and 'LastWeek'.
    - Improved error handling with descriptive messages when no events are found for the selected date range

--- MetaBond.Application/Feature/Events/Query/FilterByDateRange/FilterByDateRangeEventsQueryHandler.cs
+++ MetaBond.Application/Feature/Events/Query/FilterByDateRange/FilterByDateRangeEventsQueryHandler.cs
@@ -1,4 +1,5 @@
-﻿using MetaBond.Application.Abstractions.Messaging;
+﻿using System.Collections;
+using MetaBond.Application.Abstractions.Messaging;
 using MetaBond.Application.DTOs.Events;
 using MetaBond.Application.Interfaces.Repository;
 using MetaBond.Application.Utils;
@@ -21,15 +22,23 @@ namespace MetaBond.Application.Feature.Events.Query.FilterByDateRange
             if (status.TryGetValue((request.DateRangeFilter), out var getStatusList))
             {
                 var eventsList = await getStatusList(cancellationToken);
-                if (eventsList == null || !eventsList.Any())
+                IEnumerable<Domain.Models.Events> eventsEnumerable = eventsList.ToList();
+                
+                if (!eventsEnumerable.Any())
                 {
-                    logger.LogError("The events list is empty.");
+                    var valuesFilter = GetValueFilter();
+                    if (valuesFilter.TryGetValue((request.DateRangeFilter), out var values))
+                    {
+                        logger.LogInformation("No events found for the given filter");
+                        
+                        return ResultT<IEnumerable<EventsDto>>.Failure(Error.NotFound("404", values));
+                    }
 
+                    logger.LogError("The events list is empty due to an unknown error.");
                     return ResultT<IEnumerable<EventsDto>>.Failure(Error.Failure("400", "The list is empty"));
-
                 }
 
-                IEnumerable<EventsDto> dTos = eventsList.Select(e => new EventsDto
+                IEnumerable<EventsDto> dTos = eventsEnumerable.Select(e => new EventsDto
                 (
                     Id: e.Id,
                     Description: e.Description,
@@ -50,7 +59,8 @@ namespace MetaBond.Application.Feature.Events.Query.FilterByDateRange
             return ResultT<IEnumerable<EventsDto>>.Failure(Error.Failure("400", "No data entered"));
 
         }
-
+        
+        # region Private Methods
         private Dictionary<Domain.DateRangeFilter, Func<CancellationToken,Task<IEnumerable<Domain.Models.Events>>>> FilterByDateRange(Guid communitiesId)
         {
             return new Dictionary<Domain.DateRangeFilter, Func<CancellationToken, Task<IEnumerable<Domain.Models.Events>>>>
@@ -60,6 +70,16 @@ namespace MetaBond.Application.Feature.Events.Query.FilterByDateRange
                 { DateRangeFilter.LastWeek, async cancellationToken => await eventsRepository.FilterByDateRange(communitiesId,DateTime.UtcNow.AddDays(- 7),cancellationToken)  }
             };
         }
-
+        private Dictionary<Domain.DateRangeFilter, string> GetValueFilter()
+        {
+            return new Dictionary<DateRangeFilter, string>
+            {
+                { DateRangeFilter.LastDay, "No events found for the last day filter." },
+                { DateRangeFilter.ThreeDays, "No events found for the last 3 days filter." },
+                { DateRangeFilter.LastWeek, "No events found for the last week filter." }
+            };
+        }
+        
+        # endregion
     }
 }
