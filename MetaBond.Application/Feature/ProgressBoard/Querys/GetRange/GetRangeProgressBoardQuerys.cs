1e0556dae35b837cbbd819caf45a1a3825bc745b
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-03-22 17:28:30 -0400

    refactor: add pagination for progress entries in ProgressBoard handler

--- MetaBond.Application/Feature/ProgressBoard/Querys/GetRange/GetRangeProgressBoardQuerys.cs
+++ MetaBond.Application/Feature/ProgressBoard/Querys/GetRange/GetRangeProgressBoardQuerys.cs
@@ -6,6 +6,9 @@ namespace MetaBond.Application.Feature.ProgressBoard.Querys.GetRange
 {
     public sealed class GetRangeProgressBoardQuerys : IQuery<IEnumerable<ProgressBoardWithProgressEntryDTos>>
     {
+        public int Page { get; set; }
+        
+        public int PageSize { get; set; }
         public DateRangeType DateRangeType {  get; set; }
     }
 }
