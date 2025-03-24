a98b1c575444fece55f5e3cb22e1595f20837dad
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-03-24 11:15:38 -0400

    refactor: optimize JOIN operation for better performance

--- MetaBond.Application/Feature/Events/Query/GetEventsWithParticipationInEvent/GetEventsWithParticipationInEventQueryHandler.cs
+++ MetaBond.Application/Feature/Events/Query/GetEventsWithParticipationInEvent/GetEventsWithParticipationInEventQueryHandler.cs
@@ -2,6 +2,7 @@
 using MetaBond.Application.DTOs.Events;
 using MetaBond.Application.Interfaces.Repository;
 using MetaBond.Application.Utils;
+using MetaBond.Domain.Models;
 using Microsoft.Extensions.Logging;
 
 namespace MetaBond.Application.Feature.Events.Query.GetEventsWithParticipationInEvent;
@@ -9,44 +10,59 @@ namespace MetaBond.Application.Feature.Events.Query.GetEventsWithParticipationIn
     internal sealed class GetEventsWithParticipationInEventQueryHandler(
         IEventsRepository eventsRepository,
         ILogger<GetEventsWithParticipationInEventQueryHandler> logger)
-        : IQueryHandler<GetEventsWithParticipationInEventQuery, EventsWithParticipationInEventsDTos>
+        : IQueryHandler<GetEventsWithParticipationInEventQuery, IEnumerable<EventsWithParticipationInEventsDTos>>
     {
-        public async Task<ResultT<EventsWithParticipationInEventsDTos>> Handle(
+        public async Task<ResultT<IEnumerable<EventsWithParticipationInEventsDTos>>> Handle(
                 GetEventsWithParticipationInEventQuery request,
                 CancellationToken cancellationToken)
         {
             var events = await eventsRepository.GetByIdAsync(request.EventsId ?? Guid.Empty);
             if (events != null)
             {
-                Domain.Models.Events? eventsWithParticipationInEvents = await eventsRepository.GetEventsWithParticipationsAsync(events.Id, cancellationToken);
-                
-                if (eventsWithParticipationInEvents!.EventParticipations != null)
-                {
-                    logger.LogInformation("Event {EventId} has {Count} participations.",
-                        eventsWithParticipationInEvents.Id, eventsWithParticipationInEvents.EventParticipations.Count);
-                }
-                else
-                {
-                    logger.LogError("EventParticipation is NULL for event {EventId}.", eventsWithParticipationInEvents.Id);
-                }
-                
-                EventsWithParticipationInEventsDTos inEventsDTos = new
-                (
-                    EventsId: eventsWithParticipationInEvents.Id,
-                    Description: eventsWithParticipationInEvents.Description,
-                    Title: eventsWithParticipationInEvents.Title,
-                    DateAndTime: eventsWithParticipationInEvents.DateAndTime,
-                    EventParticipations: eventsWithParticipationInEvents.EventParticipations,
-                    CreatedAt: eventsWithParticipationInEvents.CreateAt
-                );
+                var eventsWithParticipationInEvents = await eventsRepository.GetEventsWithParticipationAsync(events.Id, cancellationToken);
 
-                logger.LogInformation("Successfully retrieved event with ID: {EventId} and its participations.", eventsWithParticipationInEvents.Id);
+                IEnumerable<Domain.Models.Events> withParticipationInEvents = eventsWithParticipationInEvents.ToList();
+               
+                if (!withParticipationInEvents.Any())
+                {
+                    logger.LogWarning("No events found with participation. Returning an empty result.");
 
-                return ResultT<EventsWithParticipationInEventsDTos>.Success(inEventsDTos);
+                    return ResultT<IEnumerable<EventsWithParticipationInEventsDTos>>.Failure(
+                        Error.NotFound("404", "No events with participation were found.")
+                    );
+                }
+                
+                var inEventsDTos = withParticipationInEvents.Select(x => new EventParticipation()
+                {
+                    ParticipationInEventId = x.EventParticipations!.Select(eventParticipation => eventParticipation.ParticipationInEventId).FirstOrDefault(),
+                    EventId = x.EventParticipations!.Select(eventParticipation => eventParticipation.EventId).FirstOrDefault(),
+                });
+                
+                IEnumerable<EventsWithParticipationInEventsDTos> participationInEventsDTosEnumerable =
+                    withParticipationInEvents.Select(x =>
+                        new EventsWithParticipationInEventsDTos(
+                            EventsId: x.Id,
+                            Description: x.Description,
+                            Title: x.Title,
+                            DateAndTime: x.DateAndTime,
+                            ParticipationInEvents: inEventsDTos.Select( ep => new ParticipationInEventBasicDTos
+                            (
+                                ParticipationInEventId:  ep.ParticipationInEventId,
+                                EventId: ep.EventId
+                            )),
+                            CreatedAt: x.CreateAt
+                        ));
+
+                IEnumerable<EventsWithParticipationInEventsDTos> eventsWithParticipationInEventsDTosEnumerable = participationInEventsDTosEnumerable.ToList();
+                logger.LogInformation("Successfully retrieved {Count} participation for Event ID: {EventId}", 
+                    eventsWithParticipationInEventsDTosEnumerable.Count(), 
+                    eventsWithParticipationInEventsDTosEnumerable.FirstOrDefault()?.EventsId ?? Guid.Empty);
+
+                return ResultT<IEnumerable<EventsWithParticipationInEventsDTos>>.Success(eventsWithParticipationInEventsDTosEnumerable);
             }
 
             logger.LogError("Event with ID: {EventId} not found.", request.EventsId);
 
-            return ResultT<EventsWithParticipationInEventsDTos>.Failure(Error.NotFound("404", $"{request.EventsId} not found"));
+            return ResultT<IEnumerable<EventsWithParticipationInEventsDTos>>.Failure(Error.NotFound("404", $"{request.EventsId} not found"));
         }
     }
