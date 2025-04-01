2f6bc7ba16b2ca4aa9dcbd2d2eba2a2e8b36ab2d
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-04-01 14:32:47 -0400

    feat: add projects tests

---
+++ MetaBond.Tests/Layers/LayersTests.cs
@@ -0,0 +1,44 @@
+using FluentAssertions;
+using NetArchTest.Rules;
+
+namespace MetaBond.Tests.Layers;
+
+public class LayersTests : BaseTests
+{
+   
+    [Fact]
+    public void Domain_Should_NotHaveDependencyOnApplication()
+    {
+        TestResult result = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn("Application")
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+    
+    [Fact]
+    public void DomainLayer_ShouldNotHaveDependencyOn_InfrastructureLayer()
+    {
+        TestResult result = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn(InfrastructureAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ApplicationLayer_ShouldNotHaveDependencyOn_InfrastructureLayer()
+    {
+        TestResult result = Types.InAssembly(ApplicationAssembly)
+            .Should()
+            .NotHaveDependencyOn(InfrastructureAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    
+    
+}
