7f2b7d5c9ca67ec3facda7cd43f91d4a2eeffded
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-04-19 20:25:40 -0400

    feat: add migration for user module

---
+++ MetaBond.Infrastructure.Persistence/Migrations/20250419225940_AddUserModule.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MetaBond.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserModule : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "AddresseeId",
+                table: "Friendship",
+                type: "uuid",
+                nullable: true);
+            
+            migrationBuilder.AddColumn<Guid>(
+                name: "RequesterId",
+                table: "Friendship",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Interests",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(25)", maxLength: 25, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PkInterest", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FirstName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Username = table.Column<string>(type: "text", nullable: true),
+                    Email = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: false),
+                    Password = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PkUser", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PkAdmin", x => x.Id);
+                    table.ForeignKey(
+                        name: "FkAdminsUserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CommunityManagers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CommunityId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PkCommunityManager", x => x.Id);
+                    table.ForeignKey(
+                        name: "FKCommunityManagerCommunity",
+                        column: x => x.CommunityId,
+                        principalTable: "Communities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FkCommunityManagersUserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CommunityUsers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CommunityId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PkCommunityUser", x => x.Id);
+                    table.ForeignKey(
+                        name: "FkCommunitiesId",
+                        column: x => x.CommunityId,
+                        principalTable: "Communities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FkCommunityUsersUserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmailConfirmationTokens",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Code = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    ExpiresAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsUsed = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PkEmailConfirmationToken", x => x.Id);
+                    table.ForeignKey(
+                        name: "FkEmailConfirmationTokensUserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Moderators",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Moderators", x => x.Id);
+                    table.ForeignKey(
+                        name: "FkModeratorsUserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserInterests",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    InterestId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserInterests", x => new { x.UserId, x.InterestId });
+                    table.ForeignKey(
+                        name: "FkInterestId",
+                        column: x => x.InterestId,
+                        principalTable: "Interests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FkUserInterestsUserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ModeratorCommunity",
+                columns: table => new
+                {
+                    ModeratorId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CommunitiesId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ModeratorCommunity", x => new { x.ModeratorId, x.CommunitiesId });
+                    table.ForeignKey(
+                        name: "FK_ModeratorCommunity_Communities_CommunitiesId",
+                        column: x => x.CommunitiesId,
+                        principalTable: "Communities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ModeratorCommunity_Moderators_ModeratorId",
+                        column: x => x.ModeratorId,
+                        principalTable: "Moderators",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Friendship_AddresseeId",
+                table: "Friendship",
+                column: "AddresseeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Friendship_RequesterId",
+                table: "Friendship",
+                column: "RequesterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Admins_UserId",
+                table: "Admins",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CommunityManagers_CommunityId",
+                table: "CommunityManagers",
+                column: "CommunityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CommunityManagers_UserId",
+                table: "CommunityManagers",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CommunityUsers_CommunityId",
+                table: "CommunityUsers",
+                column: "CommunityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CommunityUsers_UserId",
+                table: "CommunityUsers",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmailConfirmationTokens_UserId",
+                table: "EmailConfirmationTokens",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ModeratorCommunity_CommunitiesId",
+                table: "ModeratorCommunity",
+                column: "CommunitiesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Moderators_UserId",
+                table: "Moderators",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserInterests_InterestId",
+                table: "UserInterests",
+                column: "InterestId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FkAddresseeId",
+                table: "Friendship",
+                column: "AddresseeId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FkRequesterId",
+                table: "Friendship",
+                column: "RequesterId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FkAddresseeId",
+                table: "Friendship");
+
+            migrationBuilder.DropForeignKey(
+                name: "FkRequesterId",
+                table: "Friendship");
+
+            migrationBuilder.DropTable(
+                name: "Admins");
+
+            migrationBuilder.DropTable(
+                name: "CommunityManagers");
+
+            migrationBuilder.DropTable(
+                name: "CommunityUsers");
+
+            migrationBuilder.DropTable(
+                name: "EmailConfirmationTokens");
+
+            migrationBuilder.DropTable(
+                name: "ModeratorCommunity");
+
+            migrationBuilder.DropTable(
+                name: "UserInterests");
+
+            migrationBuilder.DropTable(
+                name: "Moderators");
+
+            migrationBuilder.DropTable(
+                name: "Interests");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Friendship_AddresseeId",
+                table: "Friendship");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Friendship_RequesterId",
+                table: "Friendship");
+
+            migrationBuilder.DropColumn(
+                name: "AddresseeId",
+                table: "Friendship");
+
+            migrationBuilder.DropColumn(
+                name: "RequesterId",
+                table: "Friendship");
+        }
+    }
+}
