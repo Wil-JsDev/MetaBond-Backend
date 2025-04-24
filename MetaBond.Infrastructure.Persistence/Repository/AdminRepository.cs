c191cd1e72723a9c590bd2ba0bafca92b608c3a9
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-04-24 19:31:57 -0400

    feat: add AdminRepository

---
+++ MetaBond.Infrastructure.Persistence/Repository/AdminRepository.cs
@@ -0,0 +1,26 @@
+using MetaBond.Application.Interfaces.Repository;
+using MetaBond.Application.Pagination;
+using MetaBond.Domain.Models;
+using MetaBond.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace MetaBond.Infrastructure.Persistence.Repository;
+
+public class AdminRepository(MetaBondContext metaBondContext) : GenericRepository<Admin>(metaBondContext), IAdminRepository
+{
+   public async Task<PagedResult<Admin>> GetPagedAsync(int pageNumber, int pageSize, CancellationToken cancellationToken)
+   {
+       var totalRecord = await _metaBondContext.Set<Admin>()
+           .AsNoTracking()
+           .CountAsync(cancellationToken);
+       
+       var pagedAdmin = await _metaBondContext.Set<Admin>()
+           .AsNoTracking()
+           .OrderBy(x => x.Id)
+           .Skip((pageNumber - 1) * pageSize)
+           .Take(pageSize)
+           .ToListAsync(cancellationToken);
+       
+       return new PagedResult<Admin>(pagedAdmin,pageNumber, pageSize, totalRecord);
+   }
+}
