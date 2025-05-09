06b801f2017a241463408b462d78ae5adf969cfa
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-05-08 22:46:19 -0400

    feat(user): add GetUserWithFriendshipsAsync method to retrieve user with friendships

--- MetaBond.Infrastructure.Persistence/Repository/Account/UserRepository.cs
+++ MetaBond.Infrastructure.Persistence/Repository/Account/UserRepository.cs
@@ -8,24 +8,7 @@ namespace MetaBond.Infrastructure.Persistence.Repository.Account;
 
 public class UserRepository(MetaBondContext metaBondContext) : GenericRepository<User>(metaBondContext), IUserRepository
 {
-
-    public async Task<PagedResult<User>> GetPagedAsync(int pageNumber, int pageSize,
-        CancellationToken cancellationToken)
-    {
-        var totalRecord = await _metaBondContext.Set<User>()
-            .AsNoTracking()
-            .CountAsync(cancellationToken);
-        
-        var pagedUser  = await _metaBondContext.Set<User>()
-            .AsNoTracking()
-            .OrderBy(u => u.Id)
-            .Skip((pageNumber - 1) * pageSize)
-            .Take(pageSize)
-            .ToListAsync(cancellationToken);
-
-        return new PagedResult<User>(pagedUser,pageNumber, pageSize, totalRecord);
-    }
-
+    
     public async Task<bool> EmailExistsAsync(string email, CancellationToken cancellationToken)
     {
         return await ValidateAsync(us =>  us.Email == email, cancellationToken);
@@ -89,5 +72,15 @@ public class UserRepository(MetaBondContext metaBondContext) : GenericRepository
             .AsNoTracking()
             .Where(e => e.Username!.Contains(keyword))
             .ToListAsync(cancellationToken);
+    }
+    public async Task<User?> GetUserWithFriendshipsAsync(Guid userId, CancellationToken cancellationToken)
+    {
+        return await _metaBondContext.Set<User>()
+            .AsNoTracking()
+            .Include(ep => ep.ReceivedFriendRequests)!
+                .ThenInclude(ef => ef.Requester)
+            .Include(ep => ep.ReceivedFriendRequests)!
+                .ThenInclude(ef => ef.Requester)
+            .FirstOrDefaultAsync(us =>  us.Id == userId, cancellationToken);
     }
 }
