91c1c3f9113727b7ef7b9818fd3bd60e208d4a98
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-05-08 22:30:38 -0400

    feat(friendship): add RequesterId and AddresseeId to Commands DTO

--- MetaBond.Application/Feature/Friendship/Commands/Create/CreateFriendshipValidator.cs
+++ MetaBond.Application/Feature/Friendship/Commands/Create/CreateFriendshipValidator.cs
@@ -9,5 +9,11 @@ public class CreateFriendshipValidator : AbstractValidator<CreateFriendshipComma
         RuleFor(x => x.Status)
             .NotEmpty().WithMessage("The status is required and cannot be empty or null.")
             .IsInEnum().WithMessage("The status must be a valid value.");
+
+        RuleFor(x => x.RequesterId)
+            .NotEmpty().WithMessage("The requesterId is required and cannot be empty or null.");
+        
+        RuleFor(x => x.AddresseeId)
+            .NotEmpty().WithMessage("The addresseeId is required and cannot be empty or null.");
     }
 }
