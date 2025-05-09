ed1889442dff6a17515fa1e0fc271cc6237af976
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-05-08 22:37:40 -0400

    feat: add UserDTOs and UserFriendshipDTOs for data transfer

---
+++ MetaBond.Application/DTOs/Account/User/UserFriendshipDTos.cs
@@ -0,0 +1,10 @@
+namespace MetaBond.Application.DTOs.Account.User;
+
+public sealed record UserFriendshipDTos
+(
+    Guid UserId,
+    string? FirstName,
+    string? LastName,
+    string? Username,
+    string? Photo
+);
