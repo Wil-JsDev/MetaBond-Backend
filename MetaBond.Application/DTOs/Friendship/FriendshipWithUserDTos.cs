96b5f4c8eeec2d52b95ba45abd46cefd4c6ab8e9
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-05-08 22:34:44 -0400

    feat: add FriendshipWithUserDTos

---
+++ MetaBond.Application/DTOs/Friendship/FriendshipWithUserDTos.cs
@@ -0,0 +1,14 @@
+using MetaBond.Application.DTOs.Account.User;
+using MetaBond.Domain;
+
+namespace MetaBond.Application.DTOs.Friendship;
+
+public record FriendshipWithUserDTos
+(
+    Guid FriendshipId,
+    Status Status,
+    Guid? RequesterId,
+    Guid? AddresseeId,
+    UserFriendshipDTos? User,
+    DateTime? CreatedAt    
+);
