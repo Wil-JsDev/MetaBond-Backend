9cb36fc08bb8a5c3bb0982f9f0349b7a8feaca47
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-05-28 10:38:05 -0400

    feat: add AddresseeFriendshipDTos and RequesterFriendshipDTos

---
+++ MetaBond.Application/DTOs/Friendship/RequesterFriendshipDTos.cs
@@ -0,0 +1,11 @@
+using MetaBond.Domain;
+
+namespace MetaBond.Application.DTOs.Friendship;
+
+public record RequesterFriendshipDTos
+(
+    Guid FriendshipId, 
+    Guid RequesterId,
+    string Username,
+    Status StatusFriendship
+);
