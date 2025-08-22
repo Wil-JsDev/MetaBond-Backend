7cbbb312baca37cb14147bdf43131cce4f0e4335
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-08-21 21:32:29 -0400

    chore: delete unnecessary property

--- MetaBond.Domain/Models/Communities.cs
+++ MetaBond.Domain/Models/Communities.cs
@@ -8,25 +8,19 @@ using System.Threading.Tasks;
 
 namespace MetaBond.Domain.Models;
 
-    public sealed class Communities : BaseModel
-    {
-        public string? Description { get; set; }
+public sealed class Communities : BaseModel
+{
+    public string? Description { get; set; }
 
-        public string? Category { get; set; }
+    public string? Category { get; set; }
 
-        public DateTime CreateAt { get; set; } = DateTime.UtcNow;
-    
-        public ProgressBoard? ProgressBoard { get; set; }
-    
-        public ICollection<Posts>? Posts { get; set; }
-        
-        public ICollection<CommunityUser>? CommunityUsers { get; set; } = new List<CommunityUser>();
-        
-        public ICollection<CommunityManager>? CommunityManagers { get; set; } = new List<CommunityManager>();
-        
-        public ICollection<ModeratorCommunity>? ModeratorCommunities { get; set; } = new List<ModeratorCommunity>();
-        
-        [JsonIgnore]
-        public ICollection<Events>? Events { get; set; }
-    }
+    public DateTime CreateAt { get; set; } = DateTime.UtcNow;
 
+    public ProgressBoard? ProgressBoard { get; set; }
+
+    public ICollection<Posts>? Posts { get; set; }
+
+    public ICollection<CommunityManager>? CommunityManagers { get; set; } = new List<CommunityManager>();
+
+    [JsonIgnore] public ICollection<Events>? Events { get; set; }
+}
--- MetaBond.Infrastructure.Persistence/DependencyInjection.cs
+++ MetaBond.Infrastructure.Persistence/DependencyInjection.cs
@@ -7,55 +7,48 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace MetaBond.Infrastructure.Persistence
+namespace MetaBond.Infrastructure.Persistence;
+
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static void AddPersistence(this IServiceCollection services, IConfiguration configuration)
     {
+        #region Redis
 
-        public static void AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        string connectionString = configuration.GetConnectionString("Redis")!;
+        services.AddStackExchangeRedisCache(options => { options.Configuration = connectionString; });
+
+        #endregion
+
+        #region DbContext
+
+        services.AddDbContext<MetaBondContext>(postgres =>
         {
-            
-            #region Redis
-            string connectionString = configuration.GetConnectionString("Redis")!;
-            services.AddStackExchangeRedisCache(options =>
-            {
-                options.Configuration = connectionString;
-            });
-            #endregion
-            
-            #region DbContext
-            services.AddDbContext<MetaBondContext>(postgres =>
-            {
-                postgres.UseNpgsql(configuration.GetConnectionString("MetaBondBackend"), b =>
-                {
-                    b.MigrationsAssembly("MetaBond.Infrastructure.Persistence");
-                });
-            });
+            postgres.UseNpgsql(configuration.GetConnectionString("MetaBondBackend"),
+                b => { b.MigrationsAssembly("MetaBond.Infrastructure.Persistence"); });
+        });
 
-            #endregion
+        #endregion
 
-            #region Repositories
-            services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-            services.AddTransient<ICommunitiesRepository, CommunitiesRepository>(); 
-            services.AddTransient<IEventsRepository, EventsRepository>();
-            services.AddTransient<IFriendshipRepository, FriendshipRepository>();
-            services.AddTransient<IParticipationInEventRepository, ParticipationInEventRepository>();
-            services.AddTransient<IPostsRepository, PostsRepository>();
-            services.AddTransient<IRewardsRepository, RewardsRepository>();
-            services.AddTransient<IProgressEntryRepository, ProgressEntryRepository>();
-            services.AddTransient<IProgressBoardRepository, ProgressBoardRepository>();
-            services.AddTransient<IEventParticipationRepository, EventParticipationRepository>();
-            services.AddTransient<IUserRepository, UserRepository>();
-            services.AddTransient<IAdminRepository, AdminRepository>();
-            services.AddTransient<ICommunityManagerRepository, CommunityManagerRepository>();
-            services.AddTransient<ICommunityUserRepository, CommunityUserRepository>();
-            services.AddTransient<IModeratorRepository, ModeratorRepository>();
-            services.AddTransient<IModeratorCommunityRepository, ModeratorCommunityRepository>();
-            services.AddTransient<IEmailConfirmationTokenRepository, EmailConfirmationTokenRepository>();
-            services.AddTransient<IInterestRepository, InterestRepository>();
-            services.AddTransient<IUserInterestRepository, UserInterestRepository>();
-            #endregion
+        #region Repositories
 
-        }
+        services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        services.AddTransient<ICommunitiesRepository, CommunitiesRepository>();
+        services.AddTransient<IEventsRepository, EventsRepository>();
+        services.AddTransient<IFriendshipRepository, FriendshipRepository>();
+        services.AddTransient<IParticipationInEventRepository, ParticipationInEventRepository>();
+        services.AddTransient<IPostsRepository, PostsRepository>();
+        services.AddTransient<IRewardsRepository, RewardsRepository>();
+        services.AddTransient<IProgressEntryRepository, ProgressEntryRepository>();
+        services.AddTransient<IProgressBoardRepository, ProgressBoardRepository>();
+        services.AddTransient<IEventParticipationRepository, EventParticipationRepository>();
+        services.AddTransient<IUserRepository, UserRepository>();
+        services.AddTransient<IAdminRepository, AdminRepository>();
+        services.AddTransient<ICommunityManagerRepository, CommunityManagerRepository>();
+        services.AddTransient<IEmailConfirmationTokenRepository, EmailConfirmationTokenRepository>();
+        services.AddTransient<IInterestRepository, InterestRepository>();
+        services.AddTransient<IUserInterestRepository, UserInterestRepository>();
+
+        #endregion
     }
 }
