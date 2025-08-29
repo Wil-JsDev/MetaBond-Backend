057065ea178ec83e62b3acbebed287c45c3b84dc
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-08-29 10:17:18 -0400

    feat: add community membership mapper

---
+++ MetaBond.Application/Mapper/CommunityMembershipMapper.cs
@@ -0,0 +1,49 @@
+using MetaBond.Application.DTOs.Account.CommunityMembership;
+using MetaBond.Application.DTOs.Communities;
+using MetaBond.Domain.Models;
+
+namespace MetaBond.Application.Mapper;
+
+public static class CommunityMembershipMapper
+{
+    public static CommunityMembershipDto ModelToDto(CommunityMembership model)
+    {
+        return new CommunityMembershipDto
+        (
+            User: UserMapper.MapUserDTos(model.User!),
+            Community: CommunityMapper.MapCommunitiesDTos(model.Community!),
+            Role: model.Role,
+            IsActive: model.IsActive
+        );
+    }
+
+    public static CommunityMembersDto CommunityMembersToDto(CommunityMembership communityMembers)
+    {
+        return new CommunityMembersDto
+        (
+            User: UserMapper.MapUserDTos(communityMembers.User!),
+            Role: communityMembers.Role
+        );
+    }
+
+    public static CommunitiesDTos CommunityMembershipToCommunitiesDTos(CommunityMembership communityMembership)
+    {
+        return new CommunitiesDTos
+        (
+            CommunitiesId: communityMembership.Community!.Id,
+            Name: communityMembership.Community!.Name,
+            Category: communityMembership.Community!.Category,
+            CreatedAt: communityMembership.Community!.CreateAt
+        );
+    }
+
+    public static LeaveCommunityDto LeaveCommunityToDto(CommunityMembership communityMembership)
+    {
+        return new LeaveCommunityDto
+        (
+            communityMembership.IsActive,
+            communityMembership.LeftOnUtc = DateTime.UtcNow
+        );
+    }
+    
+}
