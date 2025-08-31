3a7733a5c582008c40411bfee557b2bdf2034692
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-08-31 14:45:43 -0400

    chore: add null checks for safer handling

--- MetaBond.Application/Mapper/UserMapper.cs
+++ MetaBond.Application/Mapper/UserMapper.cs
@@ -5,10 +5,10 @@ namespace MetaBond.Application.Mapper;
 
 public static class UserMapper
 {
-    public static UserDTos MapUserDTos(User user)
+    public static UserDTos MapUserDTos(User? user)
     {
         return new UserDTos(
-            UserId: user.Id,
+            UserId: user!.Id,
             FirstName: user.FirstName,
             LastName: user.LastName,
             Username: user.Username,
