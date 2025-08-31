09ee3472096f47c3f55f5e3d9b81e1547b3f2f82
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-08-31 14:47:57 -0400

    feat(repository): add interest and user-interest repository contracts
    - Added IUserInterestRepository with methods for associating multiple interests to a user
      and bulk creation of UserInterest entities
    - Added IInterestRepository with methods for paged queries by name, by user, and existence checks
    - Included XML documentation for clarity and maintainability

--- MetaBond.Application/Interfaces/Repository/Account/IUserInterestRepository.cs
+++ MetaBond.Application/Interfaces/Repository/Account/IUserInterestRepository.cs
@@ -2,4 +2,32 @@ using MetaBond.Domain.Models;
 
 namespace MetaBond.Application.Interfaces.Repository.Account;
 
-public interface IUserInterestRepository : IGenericRepository<UserInterest>;
+public interface IUserInterestRepository : IGenericRepository<UserInterest>
+{
+    /// <summary>
+    /// Associates a collection of interests to a user.
+    /// </summary>
+    /// <remarks>
+    /// Only adds new interests that the user does not already have. 
+    /// Existing associations are not removed or updated.
+    /// </remarks>
+    /// <param name="userId">The ID of the user to associate interests with.</param>
+    /// <param name="interestIds">The list of interest IDs to associate.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>
+    /// A read-only collection of <see cref="UserInterest"/> representing the newly created associations.
+    /// </returns>
+    Task<IReadOnlyCollection<UserInterest>> AssociateInterestsToUserAsync(
+        Guid userId,
+        IEnumerable<Guid> interestIds,
+        CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Adds multiple UserInterest entities to the database.
+    /// </summary>
+    /// <param name="userInterests">A list of UserInterest entities to create.</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the operation.</param>
+    Task CreateMultipleUserInterestAsync(
+        List<UserInterest> userInterests,
+        CancellationToken cancellationToken);
+}
