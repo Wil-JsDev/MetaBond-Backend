21afc1198cf31684fcb8080f356fa8ed2bd97d67
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-08-31 14:49:25 -0400

    feat: add pagination helper

---
+++ MetaBond.Application/Helpers/PaginationHelper.cs
@@ -0,0 +1,22 @@
+using MetaBond.Application.Pagination;
+using MetaBond.Application.Utils;
+using Microsoft.Extensions.Logging;
+
+namespace MetaBond.Application.Helpers;
+public static class PaginationHelper
+{
+    public static ResultT<PagedResult<T>> ValidatePagination<T>(int pageNumber, int pageSize, ILogger logger)
+    {
+        if (pageNumber <= 0 || pageSize <= 0)
+        {
+            logger.LogWarning("Invalid pagination parameters: PageNumber={PageNumber}, PageSize={PageSize}.",
+                pageNumber, pageSize);
+
+            return ResultT<PagedResult<T>>.Failure(
+                Error.Failure("400",
+                    "Invalid pagination parameters. PageNumber and PageSize must be greater than zero."));
+        }
+
+        return null; // Indicates that it is valid
+    }
+}
