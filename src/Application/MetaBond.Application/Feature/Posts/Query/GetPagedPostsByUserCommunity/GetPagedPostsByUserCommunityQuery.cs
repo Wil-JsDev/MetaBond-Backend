6490334e932cda0152a218586d4f674781ba3100
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-10-21 21:12:33 -0400

    feat: paged query posts by user and community

---
+++ src/Application/MetaBond.Application/Feature/Posts/Query/GetPagedPostsByUserCommunity/GetPagedPostsByUserCommunityQuery.cs
@@ -0,0 +1,16 @@
+using MetaBond.Application.Abstractions.Messaging;
+using MetaBond.Application.DTOs.Posts;
+using MetaBond.Application.Pagination;
+
+namespace MetaBond.Application.Feature.Posts.Query.GetPagedPostsByUserCommunity;
+
+public sealed class GetPagedPostsByUserCommunityQuery : IQuery<PagedResult<PostsDTos>>
+{
+    public int PageNumber { get; set; }
+    
+    public int PageSize { get; set; }
+    
+    public Guid? CommunitiesId { get; set; }
+    
+    public Guid? CreatedById { get; set; }
+}
