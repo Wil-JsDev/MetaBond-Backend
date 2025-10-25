1a97182c77aeb07a0ab2fc408a95e0572c5cfb54
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-10-25 17:47:02 -0400

    feat: add messages mapper

--- src/Application/MetaBond.Application/Mapper/MessageMapper.cs
+++ src/Application/MetaBond.Application/Mapper/MessageMapper.cs
@@ -1,4 +1,5 @@
 using MetaBond.Application.DTOs.Account.Message;
+using MetaBond.Application.DTOs.Account.User;
 using MetaBond.Domain.Models;
 
 namespace MetaBond.Application.Mapper;
@@ -12,6 +13,55 @@ public static class MessageMapper
             message.Content ?? string.Empty,
             message.SentAt,
             UserMapper.MapToSenderDTos(message.Sender ?? new User())
+        );
+    }
+
+    public static MessageDto MapToMessageDto(Message message)
+    {
+        return new MessageDto(
+            MessageId: message.Id,
+            Content: message.Content ?? string.Empty,
+            UserId: message.SenderId,
+            SentAt: message.SentAt
+        );
+    }
+
+    public static MessageWithUserDTos MessageWithUserDTos(Message message, User user)
+    {
+        return new MessageWithUserDTos(
+            MessageId: message.Id,
+            Content: message.Content ?? string.Empty,
+            SentAt: message.SentAt,
+            Sender: new SenderDTos(
+                SenderId: user.Id,
+                DisplayName: user.Username,
+                Photo: user.Photo
+            )
+        );
+    }
+
+    public static MessageGeneralDTos MapToMessageGeneralDTos(Message message, User user)
+    {
+        return new MessageGeneralDTos(
+            MessageId: message.Id,
+            ChatId: message.ChatId,
+            Content: message.Content ?? string.Empty,
+            SentAt: message.SentAt,
+            IsEdited: message.IsEdited,
+            EditedAt: message.EditedAt,
+            IsDeleted: message.IsDeleted,
+            Sender: new SenderDTos(
+                SenderId: user.Id,
+                DisplayName: user.Username,
+                Photo: user.Photo
+            )
+        );
+    }
+
+    public static UpdateEditedMessageDTos MapToUpdateEditedMessageDTos(Message message)
+    {
+        return new UpdateEditedMessageDTos(
+            Content: message.Content ?? string.Empty
         );
     }
 }
